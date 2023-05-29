fa93d14424d6159978dbb0258b0908323cdb9e49
Nicolas Torres <desmondgamesyt@gmail.com>
2023-05-29 17:40:04 -0400

    categories section finished, starting all products section

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { ChevronDown, ChevronRight } from "react-feather";
 export const Home = (props) => {
+  const [catClick, setCatClick] = useState(false);
+  const handleClick = (e) => {
+    if (
+      e.currentTarget.children[1].style.display === "none" ||
+      e.currentTarget.children[1].style.display === ""
+    ) {
+      e.currentTarget.children[1].style.display = "block";
+      setCatClick(!catClick);
+      return;
+    } else if (e.currentTarget.children[1].style.display === "block") {
+      e.currentTarget.children[1].style.display = "none";
+      setCatClick(!catClick);
+      return;
+    }
+  };
+  const handleCategory = (e) => {
+    e.currentTarget.parentElement.parentElement.children[0].children[0].innerText = e.currentTarget.innerText
+    setCatClick(!catClick);
+  }
   return (
     <>
       <header className="absolute top-0 w-full">
@@ -75,18 +94,22 @@ export const Home = (props) => {
       </section>
       <section id="new-arrival">
         <div className="text-center">
-          <div className="my-12">
+          <div className="py-12">
             <h3 className="text-5xl">NEWS ARRIVALS</h3>
             <h4 className="text-3xl font-light">EXCLUSIVE CLOTHES</h4>
           </div>
-          <div className="flex flex-wrap gap-5 gap-y-20 justify-center">
+          <div className="flex flex-wrap gap-5 gap-y-5 justify-center">
             {props.dummyData?.map((product) => {
               return (
                 <div
-                  className="flex flex-col items-center w-1/6 relative"
+                  className="flex flex-col items-center w-1/6 relative  max-lg:w-1/4 max-sm:w-2/4 z-10 [&>*:nth-child(2)]:hover:opacity-100 [&>*:nth-child(2)]:hover:relative [&>*:nth-child(1)]:hover:opacity-0 [&>*:nth-child(1)]:hover:hidden transition-opacity duration-300"
                   key={product.name + Math.random()}
                 >
                   <img src={product.img}></img>
+                  <img
+                    className="absolute opacity-0 transition-opacity duration-300"
+                    src={product.imgTwo}
+                  ></img>
                   <p className="absolute z-10 top-0 right-0 px-7 py-2 text-base font-bold category text-white">
                     {product.category}
                   </p>
@@ -95,8 +118,12 @@ export const Home = (props) => {
                     ${product.price}
                   </p>
                   <div className="gap-2 flex flex-col items-center pb-20">
-                    <Link className="buy-button">Buy</Link>
-                    <Link className="personalize-button">Personalize It!</Link>
+                    <Link className="buy-button text-2xl font-bold flex justify-center w-2/4">
+                      Buy
+                    </Link>
+                    <Link className="personalize-button max-sm:text-base whitespace-nowrap text-2xl font-bold">
+                      Personalize It!
+                    </Link>
                   </div>
                 </div>
               );
@@ -104,60 +131,80 @@ export const Home = (props) => {
           </div>
         </div>
       </section>
-      <section id="search-all" className="relative overflow-hidden">
-        <div className="relative px-24">
-          <div className="pt-24 pb-8">
+      <section id="search-all" className="relative overflow-hidden pb-16">
+        <div className="flex flex-col items-center p-24 z-10 max-sm:p-12">
+          <div className="flex flex-col items-center p-12 max-sm:text-center max-sm:p-0 max-sm:py-4">
             <h4 className="text-5xl">EXPLORE ALL</h4>
             <h5 className="text-3xl font-light">SEARCH ALL CATEGORIES</h5>
           </div>
-          <div className="grid grid-cols-6 grid-rows-2 gap-5 z-10 relative grid-categories">
-            <div className="row-span-2 ">
-              <img className="object-cover h-full" src="/img/bag.webp"></img>
-              <Link className="absolute bottom-2 left-4">See HandBags</Link>
-            </div>
-            <div className="col-span-2">
-              <img
-                className="w-full object-cover"
-                src="/img/shirtcat.png"
-              ></img>
-              <Link className="absolute bottom-2 left-4">See T-shirts</Link>
-            </div>
-            <div className="row-span-2">
-              <img
-                className="object-cover h-full"
-                src="/img/stickers.png"
-              ></img>
-              <Link className="absolute bottom-2 left-4">See Stickers</Link>
-            </div>
-            <div>
-              <img
-                className="object-cover h-full"
-                src="/img/creativeshirt.png"
-              ></img>
-              <Link className="absolute bottom-2 left-4">See Hoddies</Link>
-            </div>
-            <div className="row-span-2">
-              <img className="h-full object-cover" src="/img/jacket.webp"></img>
-              <Link className="absolute bottom-2 left-4">See Jackets</Link>
-            </div>
-            <div className="col-span-2">
-              <img className="h-full object-cover" src="/img/gap.webp"></img>
-              <Link className="absolute bottom-2 left-4">See Gaps</Link>
-            </div>
-            <div>
-              <img
-                className="h-full object-cover"
-                src="/img/creativebag.png"
-              ></img>
-              <Link className="absolute bottom-2 left-4">See Bags</Link>
+          <div className="flex items-center z-10">
+            <div className="sm:grid grid-cols-3 grid-rows-4 w-full grid-cat gap-3 max-sm:grid-cols-2 max-sm:flex max-sm:flex-col max-sm:gap-4">
+              {props.categories?.map((category, i) => {
+                return (
+                  <div
+                    key={category.name}
+                    className={
+                      i === 0 || i === 3 || i === 4
+                        ? "row-span-2 overflow-hidden rounded-xl relative max-sm:col-span-2 max-sm:row-span-1"
+                        : undefined || i === 1 || i === 2
+                        ? "col-span-2 overflow-hidden rounded-xl relative"
+                        : undefined || i
+                        ? "overflow-hidden rounded-xl relative"
+                        : undefined
+                    }
+                  >
+                    <img
+                      className="h-full w-full object-cover rounded-xl brightness-75 hover:scale-110 transition-transform duration-500 hover:transition-transform duration-500"
+                      src={category.img}
+                    ></img>
+                    <Link className="absolute bottom-0 left-0 py-4 px-7 bg-main rounded-tr-3xl font-bold text-xl flex items-center gap-3 hover:opacity-80">
+                      Go to {category.name}{" "}
+                      <ChevronRight className="bg-white rounded-full bg-opacity-30	"></ChevronRight>
+                    </Link>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
-        <div className="overflow-hidden">
-          <img
-            className="shirtbackground overflow-hidden"
-            src="/img/shirtbackground.webp"
-          ></img>
+      </section>
+      <section id="all-products">
+        <div className="px-24 pt-16">
+          <div className="py-5">
+            <h5 className="text-5xl">ALL PRODUCTS</h5>
+          </div>
+          <div className="flex gap-12">
+            <div
+              className="bg-gray-200 w-44 py-2 px-5 flex items-center justify-center  cursor-pointer relative"
+              onClick={handleClick}
+            >
+              <div className="flex items-center text-2xl font-bold text-gray-800 select-none">
+              <p>
+                Category
+              </p>
+              <ChevronDown></ChevronDown>
+              </div>
+              <ul
+                className="absolute top-full left-0 text-2xl bg-gray-200 w-full pl-2 font-normal text-gray-700"
+                style={{ display: "none" }}
+              >
+                {props.categories?.map((category) => {
+                  return <li key={category.name} onClick={handleCategory}>{category.name}</li>;
+                })}
+              </ul>
+            </div>
+            <div className="bg-gray-200 w-36 py-2 px-5 flex items-center justify-center  cursor-pointer">
+              <p className="flex items-center text-2xl font-bold text-gray-800 select-none">
+                Color<ChevronDown></ChevronDown>
+              </p>
+            </div>
+            <div className="bg-gray-200 w-36 py-2 px-5 flex items-center justify-center  cursor-pointer">
+              <p className="flex items-center text-2xl font-bold text-gray-800 select-none">
+                Price<ChevronDown></ChevronDown>
+              </p>
+            </div>
+          </div>
+          <div></div>
         </div>
       </section>
     </>
